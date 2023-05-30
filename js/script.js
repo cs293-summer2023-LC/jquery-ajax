aadefde8c58f5ff61f590d9d9b5a1620f0fd08dc
Jeremy McWilliams <jeremym@lclark.edu>
2023-05-29 21:52:59 -0700

    done

---
+++ js/script.js
@@ -0,0 +1,64 @@
+/* Sample AJAX calls   */
+/*
+
+function myFunction(){
+
+    $.ajax({
+        method: "GET",
+        url: "https://opentdb.com/api.php",
+        data: { key1: "val1", key2: "val2"  }  
+        // use data if needed, equates to https://opentdb.com/api.php?key1=val1&key2=val2 
+        // data values could be arguments passed to the function
+    })
+        .done(function(response) {
+            //response (or whatever you want to call the variable) is the API response
+            
+            // first console.log the response to see it
+            console.log(response);
+
+            // while the AJAX call is asynchronous, code in this section is back to synchronous.
+
+
+                        
+    });
+}
+
+*/
+
+/* Write event handler and function for getting the token here */
+
+
+function getToken(){
+
+
+
+}
+
+
+
+
+/* Write event handler and function for getting a sports question, 
+and displaying its question and correct answer here */
+
+
+function getSportsQ(){
+
+
+
+
+}
+
+
+
+
+/*Write event handler and function for getting a history question,
+and displaying the question plus possible answers as radio buttons here */
+
+
+function getHistoryQ(){
+
+
+
+    
+}
+
